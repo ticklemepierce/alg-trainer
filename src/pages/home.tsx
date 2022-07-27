fee346e1d6e18928616afe26ad7e9d3083d8dc9a
Pierce <pierce.tickle@gmail.com>
2022-07-26 22:21:39 -0400

    work

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import { PuzzleCard } from '../components/puzzle-card';
+// import Box from '@mui/material/Box';
+// import { PuzzleCard } from '../components/puzzle-card';
 
-const PUZZLES = [2, 3, 4, 5];
+// const PUZZLES = [2, 3, 4, 5];
 
 export const HomePage = () => (
-  <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-    {PUZZLES.map(puzzle => <PuzzleCard puzzle={puzzle} title={`${puzzle}x${puzzle}`} link={`/puzzle/${puzzle}`}/>)}
-  </Box>
+  <h1>TEST</h1>
+  // <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+  //   {PUZZLES.map(puzzle => <PuzzleCard puzzle={puzzle} title={`${puzzle}x${puzzle}`} link={`/puzzle/${puzzle}`}/>)}
+  // </Box>
 );
