e99cc469d4a49550c6645fe08b1599077ce8edbb
Pierce <pierce.tickle@gmail.com>
2022-07-26 22:31:49 -0400

    oops

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,10 @@
-// import Box from '@mui/material/Box';
-// import { PuzzleCard } from '../components/puzzle-card';
+import Box from '@mui/material/Box';
+import { PuzzleCard } from '../components/puzzle-card';
 
-// const PUZZLES = [2, 3, 4, 5];
+const PUZZLES = [2, 3, 4, 5];
 
 export const HomePage = () => (
-  <h1>TEST</h1>
-  // <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-  //   {PUZZLES.map(puzzle => <PuzzleCard puzzle={puzzle} title={`${puzzle}x${puzzle}`} link={`/puzzle/${puzzle}`}/>)}
-  // </Box>
+  <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+    {PUZZLES.map(puzzle => <PuzzleCard puzzle={puzzle} title={`${puzzle}x${puzzle}`} link={`/puzzle/${puzzle}`}/>)}
+  </Box>
 );
