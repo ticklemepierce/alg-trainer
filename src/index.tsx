de95ead2032bfbeb4af08e17bc7e7b5c125113d7
Pierce <pierce.tickle@gmail.com>
2022-07-26 22:09:59 -0400

    hello world

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 
-import { HomePage } from './pages/home';
-import { PuzzlePage } from './pages/puzzle';
+// import { HomePage } from './pages/home';
+// import { PuzzlePage } from './pages/puzzle';
 import {
   BrowserRouter,
   Routes,
@@ -10,19 +10,20 @@ import {
 } from "react-router-dom";
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage /> }/>
-      <Route path="/puzzle/3">
-        <Route path="" element={<PuzzlePage puzzle={4} />} />
-        <Route path=":step" element={<h1>I'm a step page test</h1>} />
-      </Route>
-      <Route path="/puzzle/4">
-        <Route path="" element={<PuzzlePage puzzle={4}/> } />
-        <Route path=":step" element={<h1>I'm a step page test</h1>} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <h1>Hello World</h1>
+  // <BrowserRouter>
+  //   <Routes>
+  //     <Route path="/" element={<HomePage /> }/>
+  //     <Route path="/puzzle/3">
+  //       <Route path="" element={<PuzzlePage puzzle={4} />} />
+  //       <Route path=":step" element={<h1>I'm a step page test</h1>} />
+  //     </Route>
+  //     <Route path="/puzzle/4">
+  //       <Route path="" element={<PuzzlePage puzzle={4}/> } />
+  //       <Route path=":step" element={<h1>I'm a step page test</h1>} />
+  //     </Route>
+  //   </Routes>
+  // </BrowserRouter>
 )
 
 ReactDOM.render(<StrictMode><App /></StrictMode>, document.getElementById('root'));
