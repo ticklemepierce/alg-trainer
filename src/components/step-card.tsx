1bea9413a492e4773c507fa68ffc6e4629bb7b95
Pierce <pierce.tickle@gmail.com>
2022-07-27 23:59:47 -0400

    swap img host

--- src/components/step-card.tsx
+++ src/components/step-card.tsx
@@ -6,14 +6,13 @@ import { Link } from 'react-router-dom';
 import { IPuzzle, IStep } from '../puzzles';
 
 export const StepCard = ({ step }: {step: IStep | IPuzzle }) => {
-  const
-   imgUrl = `http://cube.rider.biz/visualcube.php?fmt=svg&size=200${step.visualCubeParams}`;
+  const imgUrl = `https://cubiclealgdbimagegen.azurewebsites.net/generator?${step.visualCubeParams}`
   return (
     <Card sx={{ maxWidth: 200, textAlign: 'center', m: 2 }}>
       <CardActionArea>
         <Link to={step.slug} style={{ textDecoration: "none", color: "inherit" }}>
           <CardContent sx={{ p: 0, '&:last-child': { p: 0 }}}>
-            <img src={imgUrl} />
+            <img src={imgUrl} height={200} width={200} />
             <Typography variant="h5" component="div" p="10">
               {step.displayName}
             </Typography>
--- src/puzzles.ts
+++ src/puzzles.ts
@@ -13,40 +13,40 @@ export const Puzzles: IPuzzle[] = [
   {
     displayName: '3x3x3',
     slug: '/puzzle/333', // TODO hoist /puzzle to the router
-    visualCubeParams: '&pzl=3',
+    visualCubeParams: '&puzzle=3',
     steps: [{
       displayName: 'F2L',
-      visualCubeParams: '&pzl=3&stage=f2l',
+      visualCubeParams: '&puzzle=3&stage=f2l',
       slug: 'f2l',
     }, {
       displayName: 'OLL',
-      visualCubeParams: '&pzl=3&stage=oll',
+      visualCubeParams: '&puzzle=3&stage=oll',
       slug: 'oll',
       steps: [{
         displayName: 'All',
-        visualCubeParams: '&pzl=3&stage=oll',
+        visualCubeParams: '&puzzle=3&stage=oll',
         slug: 'all',
       }, {
         displayName: 'Dots',
-        visualCubeParams: '&pzl=3&stage=oll',
+        visualCubeParams: '&puzzle=3&stage=oll',
         slug: 'dots',
       }]
     }, {
       displayName: 'PLL',
-      visualCubeParams: '&pzl=3&stage=pll',
+      visualCubeParams: '&puzzle=3&stage=pll',
       slug: 'pll',
     }],
   }, {
     displayName: '4x4x4',
     slug: '/puzzle/444', // TODO hoist /puzzle to the router
-    visualCubeParams: '&pzl=4',
+    visualCubeParams: '&puzzle=4',
     steps: [{
       displayName: 'F3L',
-      visualCubeParams: '&pzl=4',
+      visualCubeParams: '&puzzle=4',
       slug: 'f3l',
     }, {
       displayName: 'cross edges',
-      visualCubeParams: '&pzl=4&fd=nnnnnnnnnnnnndnnnnnnnrrnnrrnnnnnnfnnnnnnnnnnnfnntddtdttddttdtddt',
+      visualCubeParams: '&puzzle=4&fd=nnnnnnnnnnnnndnnnnnnnrrnnrrnnnnnnfnnnnnnnnnnnfnntddtdttddttdtddt',
       slug: 'cross_edges'
     }],
   }
