ea975646cdfc926200ce0fca8da740ceff7933c6
Pierce <pierce.tickle@gmail.com>
2022-08-29 22:57:04 -0400

    create pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,65 @@
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
 
-import { HomePage } from './pages/home';
-import { StepPage } from './pages/step';
-import { AlgsPage } from './pages/algs';
-import { Trainer } from './components/trainer';
-import { AlgsList } from './components/algs-list';
-import {
-  HashRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { HomePage } from "./pages/home";
+import { StepPage } from "./pages/step";
+import { AlgsPage } from "./pages/algs";
+import { Trainer } from "./components/trainer";
+import { AlgsList } from "./components/algs-list";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-import { Puzzles, Step, ISubStep, isSubStep } from './puzzles';
+import { Puzzles, Step, ISubStep, isSubStep } from "./puzzles";
 
 const createStepRoutes = (step: Step) => (
   <Route path={`${step.slug}`} key={step.slug}>
-    {isSubStep(step) ? 
+    {isSubStep(step) ? (
       <>
         <Route path="" element={<StepPage step={step} />} />
-        {step.steps && step.steps.map(substep => (createStepRoutes(substep)))}
-      </> :
+        {step.steps && step.steps.map((substep) => createStepRoutes(substep))}
+      </>
+    ) : (
       <Route path="" element={<AlgsPage step={step} />}>
         <Route path="" element={<AlgsList />} />
         <Route path="trainer" element={<Trainer />} />
       </Route>
-    }
+    )}
   </Route>
-)
+);
 
 const createPuzzleRoutes = (puzzle: ISubStep) => (
   <Route path={`${puzzle.slug}`} key={puzzle.slug}>
     <Route path="" element={<StepPage step={puzzle} />} />
-    {puzzle.steps.map(step => (createStepRoutes(step)))}
+    {puzzle.steps.map((step) => createStepRoutes(step))}
   </Route>
-)
+);
 
 const App = () => (
   <HashRouter>
     <Routes>
-      <Route path="/" element={<HomePage /> }/>
-      {Puzzles.map(puzzle => (createPuzzleRoutes(puzzle)))}
+      <Route path="/" element={<HomePage />} />
+      {Puzzles.map((puzzle) => createPuzzleRoutes(puzzle))}
     </Routes>
   </HashRouter>
-)
+);
 
-ReactDOM.render(<StrictMode><App /></StrictMode>, document.getElementById('root'));
+if ("serviceWorker" in navigator) {
+  console.log(new URL("../service-worker.js", import.meta.url));
+  navigator.serviceWorker
+    .register(new URL("../service-worker.js", import.meta.url), {
+      type: "module",
+    })
+    .then((registration) => {
+      console.log("SW Registered");
+      console.log(registration);
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+}
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
