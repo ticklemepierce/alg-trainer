778abd05f4bfc66c59b4782def2ac37db89552e3
Pierce <pierce.tickle@gmail.com>
2022-09-08 19:34:54 -0400

    edit to include f2l cases offline

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,7 @@ import { manifest, version } from "@parcel/service-worker";
 async function install() {
   const cache = await caches.open(version);
   await cache.addAll(manifest);
+  await cache.add("f2l.json");
 }
 addEventListener("install", (e) => e.waitUntil(install()));
 
