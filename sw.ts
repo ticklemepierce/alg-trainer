f26716d6e8d588b465c3efca7386961b2e011305
Pierce <pierce.tickle@gmail.com>
2023-01-24 23:45:00 -0500

    new cache strategy

--- sw.ts
+++ sw.ts
@@ -1,9 +1,27 @@
-import { precacheAndRoute } from "workbox-precaching";
-import { createHandlerBoundToURL } from "workbox-precaching";
-import { NavigationRoute, registerRoute } from "workbox-routing";
+// import { precacheAndRoute } from "workbox-precaching";
+// import { createHandlerBoundToURL } from "workbox-precaching";
+// import { NavigationRoute, registerRoute } from "workbox-routing";
 
-precacheAndRoute((self as any).__WB_MANIFEST);
+// precacheAndRoute((self as any).__WB_MANIFEST);
 
-const handler = createHandlerBoundToURL("index.html");
-const navigationRoute = new NavigationRoute(handler);
-registerRoute(navigationRoute);
+// const handler = createHandlerBoundToURL("index.html");
+// const navigationRoute = new NavigationRoute(handler);
+// registerRoute(navigationRoute);
+
+import {
+  pageCache,
+  imageCache,
+  staticResourceCache,
+  googleFontsCache,
+  offlineFallback,
+} from "workbox-recipes";
+
+pageCache();
+
+googleFontsCache();
+
+staticResourceCache();
+
+imageCache();
+
+offlineFallback();
