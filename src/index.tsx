316a85c6c3069ab568670c7268c5a16eb6b6c862
Pierce <pierce.tickle@gmail.com>
2023-01-24 19:34:29 -0500

    add icons

--- src/index.tsx
+++ src/index.tsx
@@ -50,7 +50,19 @@ const App = () => (
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="theme-color" content="#1976d2" />
       <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <link rel="apple-touch-icon" href="icons/icon-192.png" />
+      <link rel="apple-touch-icon" href="static/icons/icon-192.png" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="static/icons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="static/icons/favicon-16x16.png"
+      />
       <link rel="manifest" href="manifest.json" />
 
       <title>Alg Trainer</title>
