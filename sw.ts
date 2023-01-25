af3db42ff0ff7514e99e5da1eb254961f77c0d37
Pierce <pierce.tickle@gmail.com>
2023-01-24 23:25:57 -0500

    path update

--- sw.ts
+++ sw.ts
@@ -4,6 +4,6 @@ import { NavigationRoute, registerRoute } from "workbox-routing";
 
 precacheAndRoute((self as any).__WB_MANIFEST);
 
-const handler = createHandlerBoundToURL("/index.html");
+const handler = createHandlerBoundToURL("index.html");
 const navigationRoute = new NavigationRoute(handler);
 registerRoute(navigationRoute);
