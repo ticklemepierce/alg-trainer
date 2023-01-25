6b2e7c0cd23cce185f7c63cd72d795e76797776a
Pierce <pierce.tickle@gmail.com>
2023-01-24 22:45:57 -0500

    update sw

--- src-sw.js
+++ src-sw.js
@@ -2,5 +2,13 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
 );
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+workbox.precaching.precacheAndRoute(
+  self.__WB_MANIFEST.filter((file) => !file.url.includes("index.html"))
+);
+
 workbox.precaching.cleanupOutdatedCaches();
+
+workbox.routing.registerRoute(
+  ({ url }) => url.pathname.includes("index.html"),
+  new workbox.NetworkFirst()
+);
