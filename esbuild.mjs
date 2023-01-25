ac650124e35abc159be42e1fe5184c2746a8c085
Pierce <pierce.tickle@gmail.com>
2023-01-24 20:56:59 -0500

    typo fix

--- esbuild.mjs
+++ esbuild.mjs
@@ -25,7 +25,7 @@ const _injectManifest = () => ({
         maximumFileSizeToCacheInBytes: 5000000,
         swSrc: "src-sw.js",
         swDest: "dist/service-worker.js",
-        templatedUrls: {
+        templatedURLs: {
           "/": ["index.html"],
         },
       });
